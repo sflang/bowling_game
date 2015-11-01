64450746a40b1a0eee55c0b7600567c41a2a2a89
Steve Lang <steveflang@gmail.com>
2015-11-01 03:31:41 +0000

    refactored indentation for readability

--- frame.rb
+++ frame.rb
@@ -1,32 +1,32 @@
 class Frame
 	
-	attr_accessor :ball_scores, :ball_score_multipliers
+  attr_accessor :ball_scores, :ball_score_multipliers
 
-    #initialize ball scores and reset score multipliers
-	def init_ball_scores(scores)
+  #initialize ball scores and reset score multipliers
+  def init_ball_scores(scores)
 
-		self.ball_scores = scores
-		self.ball_score_multipliers = scores.map { |i| 1}
-	end
+    self.ball_scores = scores
+	self.ball_score_multipliers = scores.map { |i| 1}
+  end
     
-    #multiply each ball score by its mutiplier and sum result
-	def score
+  #multiply each ball score by its mutiplier and sum result
+  def score
 
-	  (0...self.ball_scores.length).inject(0) do |sum, i| 
-	  	sum += self.ball_scores[i] * self.ball_score_multipliers[i]
-	  end
+    (0...self.ball_scores.length).inject(0) do |sum, i| 
+	  sum += self.ball_scores[i] * self.ball_score_multipliers[i]
 	end
+  end
 
-	def strike?
+  def strike?
 
-		self.ball_scores[0] == 10
-	end
+    self.ball_scores[0] == 10
+  end
 
-	def spare?
+  def spare?
 
-		self.ball_scores.length > 1                     &&
-		self.ball_scores[0] + self.ball_scores[1] == 10
-	end
+    self.ball_scores.length > 1                     &&
+	self.ball_scores[0] + self.ball_scores[1] == 10
+  end
 end
 
 
--- game.rb
+++ game.rb
@@ -21,26 +21,26 @@ class Game
 	#update the ball_score_multipliers based on strikes/spares
 	def update_frame_scores
       
-      self.frames[0..8].each_with_index do |frame, i|
-      	#if strike or spare, increment multiplier for the first ball in the next frame
-	  	if frame.strike? || frame.spare?
+    self.frames[0..8].each_with_index do |frame, i|
+    #if strike or spare, increment multiplier for the first ball in the next frame
+	  if frame.strike? || frame.spare?
 
-          self.frames[i+1].ball_score_multipliers[0] += 1
-        end
+      self.frames[i+1].ball_score_multipliers[0] += 1
+    end
           
-        #if strike, increment multiplier for the second ball, which may be located
-        #in the next frame, or the next next frame
-      	if frame.strike?
+    #if strike, increment multiplier for the second ball, which may be located
+    #in the next frame, or the next next frame
+    if frame.strike?
             
-          self.frames[i+1].ball_score_multipliers.length > 1 ?
-            self.frames[i+1].ball_score_multipliers[1] += 1  : 
-            self.frames[i+2].ball_score_multipliers[0] += 1
-        end
+      self.frames[i+1].ball_score_multipliers.length > 1 ?
+        self.frames[i+1].ball_score_multipliers[1] += 1  : 
+        self.frames[i+2].ball_score_multipliers[0] += 1
       end
     end
+  end
 
-    def final_score
+  def final_score
 
-      self.frames.inject(0) {|sum, frame| sum += frame.score}
-    end
+    self.frames.inject(0) {|sum, frame| sum += frame.score}
+  end
 end
